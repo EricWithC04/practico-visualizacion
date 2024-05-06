d75c419b20ab85b8733181f2570e9994607a9ae0
EricWithC04 <negativegame117@gmail.com>
2024-05-06 20:09:14 -0300

    feat: victory try

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import './App.css'
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory'
 
 function App() {
   
+  const rotatedLabelStyle = {
+    tickLabels: {
+      angle: -45, 
+      textAnchor: "end"
+    }
+  }
 
   return (
     <>
       <h1>Hello World</h1>
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis
+          tickValues={[8, 2, 3, 6, 5, 6, 7, 8, 8, 1]}
+          tickFormat={["8", "2", "3", "6", "5", "6", "7", "8", "8", "1"]}
+          style={{
+            axis: { stroke: "#ffffff" },
+            ticks: { stroke: "#ffffff" },
+            tickLabels: { fill: "#ffffff" },
+          }}
+        />
+        <VictoryAxis dependentAxis/>
+        <VictoryBar
+          style={{
+            data: { fill: "tomato" },
+            }}
+        />
+      </VictoryChart>
     </>
   )
 }
