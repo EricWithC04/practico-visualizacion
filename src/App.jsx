9d0a98e2fa2eb0cf9522fb3467ef3ba4bbc8b03d
MrTex09 <bagente24@gmail.com>
2024-05-07 10:01:30 -0300

    se actualizo los visuales

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import PieChart from './components/PieChart';
 import BarChart from './components/BarChart';
-import { 
-  dataReader, 
-  mostRepeatedChannels, 
-  mostRepeatedCategories, 
-  higherAVGlikes, 
-  higherAVGdisLikes } from './utils/dataReader'
+import { dataReader, mostRepeatedChannels, mostRepeatedCategories, higherAVGlikes, higherAVGdisLikes } from './utils/dataReader'
 import "./components/App.css"
-// Importar los datos de un archivo separado idealmente
 import { pieData2, barData1, barData2 } from './data';
 
 function App() {
@@ -29,15 +23,14 @@ function App() {
           {
               label: 'Canales con más videos',
               data: data,
-              // backgroundColor: 'rgba(255, 99, 132, 0.6)',
-              // borderColor: 'rgba(255, 99, 132, 1)',
+              backgroundColor: 'rgba(54, 162, 235, 0.6)',
+              borderColor: 'rgba(54, 162, 235, 1)',
               borderWidth: 1,
           },
       ],
     }
-
     setDataChannels(completeData)
-  }, [])
+  }, []);
 
   useEffect(() => {
     const categories = mostRepeatedCategories()
@@ -50,15 +43,14 @@ function App() {
           {
               label: 'Categorias más vistas',
               data: data,
-              // backgroundColor: 'rgba(255, 99, 132, 0.6)',
-              // borderColor: 'rgba(255, 99, 132, 1)',
+              // backgroundColor: 'rgba(75, 192, 192, 0.6)',
+              // borderColor: 'rgba(75, 192, 192, 1)',
               borderWidth: 1,
           },
       ],
     }
-
     setDataCategories(completeData)
-  }, [])
+  }, []);
 
   useEffect(() => {
     const likes = higherAVGlikes()
@@ -71,15 +63,14 @@ function App() {
           {
               label: 'Canales con mejor promedio de likes',
               data: data,
-              backgroundColor: 'rgba(255, 99, 132, 0.6)',
-              borderColor: 'rgba(255, 99, 132, 1)',
+              backgroundColor: 'rgba(79, 238, 77, 0.58)',
+              borderColor: 'rgba(36, 244, 33, 0.89)',
               borderWidth: 1,
           },
       ],
     }
-
     setDataLikes(completeData)
-  }, [])
+  }, []);
 
   useEffect(() => {
     const dislikes = higherAVGdisLikes()
@@ -92,35 +83,36 @@ function App() {
           {
               label: 'Canales con mayor promedio de dislikes',
               data: data,
-              // backgroundColor: 'rgba(255, 99, 132, 0.6)',
-              // borderColor: 'rgba(255, 99, 132, 1)',
+              backgroundColor: 'rgba(255, 99, 132, 0.6)',  // Rojo
+              borderColor: 'rgba(255, 99, 132, 1)',        // Borde rojo más fuerte
               borderWidth: 1,
           },
       ],
     }
-
     setDataDislikes(completeData)
-  }, [])
+  }, []);
 
-    return (
-        <div className="App">
-            <h1>Dashboard</h1>
-            <div className="chart-container">
-                <div className="sub-container">
-                <BarChart data={dataDislikes.hasOwnProperty("labels") ? dataDislikes : barData2} />
-                </div>
-                <div className="sub-container">
-                    <PieChart data={dataCategories.hasOwnProperty("labels") ? dataCategories : pieData2} />
-                </div>
-                <div className="sub-container">
-                    <BarChart data={dataLikes.hasOwnProperty("labels") ? dataLikes : barData1} />
-                </div>
-                <div className="sub-container">
-                    <BarChart data={dataChannels.hasOwnProperty("labels") ? dataChannels : barData2} />
-                </div>
-            </div>
-        </div>
-    );
+  return (
+      <div className="App">
+          <h1>Dashboard</h1>
+          <div className="chart-container">
+              <div  className="sub-container sub-container-dislike">
+                <BarChart data={dataDislikes.hasOwnProperty("labels") ? dataDislikes : barData1} />
+              </div>
+              <div className="sub-container sub-container-like">
+                  <BarChart data={dataLikes.hasOwnProperty("labels") ? dataLikes : barData1} />
+              </div>
+              <div className="sub-container">
+                  <BarChart data={dataChannels.hasOwnProperty("labels") ? dataChannels : barData2} />
+              </div>
+              <div className="sub-container">
+                  <PieChart data={dataCategories.hasOwnProperty("labels") ? dataCategories : pieData2} />
+              </div>
+
+
+          </div>
+      </div>
+  );
 }
 
 export default App;
