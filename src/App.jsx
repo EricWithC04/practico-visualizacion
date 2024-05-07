f8e4c1032aa0cdecc2a5e5cc4b11c4a0505d6cc3
EricWithC04 <negativegame117@gmail.com>
2024-05-07 01:03:42 -0300

    feat: graphics with the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PieChart from './components/PieChart';
 import BarChart from './components/BarChart';
 import { 
@@ -12,6 +12,96 @@ import "./components/App.css"
 import { pieData1, pieData2, barData1, barData2 } from './data';
 
 function App() {
+
+  const [dataChannels, setDataChannels] = useState({})
+  const [dataCategories, setDataCategories] = useState({})
+  const [dataLikes, setDataLikes] = useState({})
+  const [dataDislikes, setDataDislikes] = useState({})
+
+  useEffect(() => {
+    const channels = mostRepeatedChannels()
+    const labels = Object.keys(channels)
+    const data = Object.values(channels)
+
+    const completeData = {
+      labels: labels,
+      datasets: [
+          {
+              label: 'Canales con más videos',
+              data: data,
+              // backgroundColor: 'rgba(255, 99, 132, 0.6)',
+              // borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+          },
+      ],
+    }
+
+    setDataChannels(completeData)
+  }, [])
+
+  useEffect(() => {
+    const categories = mostRepeatedCategories()
+    const labels = Object.keys(categories)
+    const data = Object.values(categories)
+
+    const completeData = {
+      labels: labels,
+      datasets: [
+          {
+              label: 'Categorias más vistas',
+              data: data,
+              // backgroundColor: 'rgba(255, 99, 132, 0.6)',
+              // borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+          },
+      ],
+    }
+
+    setDataCategories(completeData)
+  }, [])
+
+  useEffect(() => {
+    const likes = higherAVGlikes()
+    const labels = Object.keys(likes)
+    const data = Object.values(likes)
+
+    const completeData = {
+      labels: labels,
+      datasets: [
+          {
+              label: 'Canales con mejor promedio de likes',
+              data: data,
+              backgroundColor: 'rgba(255, 99, 132, 0.6)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+          },
+      ],
+    }
+
+    setDataLikes(completeData)
+  }, [])
+
+  useEffect(() => {
+    const channels = mostRepeatedChannels()
+    const labels = Object.keys(channels)
+    const data = Object.values(channels)
+
+    const completeData = {
+      labels: labels,
+      datasets: [
+          {
+              label: 'Canales con más videos',
+              data: data,
+              backgroundColor: 'rgba(255, 99, 132, 0.6)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1,
+          },
+      ],
+    }
+
+    setDataChannels(completeData)
+  }, [])
+
     return (
         <div className="App">
             <h1>Dashboard</h1>
@@ -20,13 +110,13 @@ function App() {
                     <PieChart data={pieData1} />
                 </div>
                 <div className="sub-container">
-                    <PieChart data={pieData2} />
+                    <PieChart data={dataCategories.hasOwnProperty("labels") ? dataCategories : pieData2} />
                 </div>
                 <div className="sub-container">
-                    <BarChart data={barData1} />
+                    <BarChart data={dataLikes.hasOwnProperty("labels") ? dataLikes : barData1} />
                 </div>
                 <div className="sub-container">
-                    <BarChart data={barData2} />
+                    <BarChart data={dataChannels.hasOwnProperty("labels") ? dataChannels : barData2} />
                 </div>
             </div>
         </div>
