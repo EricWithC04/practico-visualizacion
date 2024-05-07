8a25263cc8a8ea7c1aa148ea491014552db4be1b
EricWithC04 <negativegame117@gmail.com>
2024-05-06 22:12:16 -0300

    feat: function mostRepeatedChannels

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
-import { dataReader } from './utils/dataReader'
+import { dataReader, mostRepeatedChannels } from './utils/dataReader'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    dataReader()
+    mostRepeatedChannels()
   }, [])
 
   return (
